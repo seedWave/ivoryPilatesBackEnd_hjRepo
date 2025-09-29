9783a9b14da2dbb03e8d3e060feb942b70815761
nks <knsol19921@gmail.com>
2025-09-29 21:25:00 +0900

    프론트 테스트용 모든 테이블 조회 쿼리 TODO: 삭제

---
+++ src/main/java/com/dogfeetbirdfeet/ivorypilatesbackend/mapper/test/TestMapper.java
@@ -0,0 +1,18 @@
+package com.dogfeetbirdfeet.ivorypilatesbackend.mapper.test;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author nks
+ * 프론트 테스트를 위한 임시 Mapper
+ */
+@Mapper
+public interface TestMapper {
+
+	// TODO 테스트용으로 SQLInjection 되니까, 오픈 전 반드시 삭제
+	List<Map<String, Object>> findAnythingByTableName(@Param("tableName") String tableName);
+}
---
+++ src/main/java/com/dogfeetbirdfeet/ivorypilatesbackend/service/test/TestService.java
@@ -0,0 +1,60 @@
+package com.dogfeetbirdfeet.ivorypilatesbackend.service.test;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.springframework.stereotype.Service;
+
+import com.dogfeetbirdfeet.ivorypilatesbackend.mapper.test.TestMapper;
+
+/**
+ * @author nks
+ * 테스트용 모든 테이블 반환
+ */
+@Service
+public class TestService {
+
+	private final TestMapper testMapper;
+
+	public TestService(TestMapper testMapper) {
+		this.testMapper = testMapper;
+	}
+
+	public List<?> getTableInfo(String tableName) {
+
+		return testMapper.findAnythingByTableName(returnCamelCaseByTableName(tableName));
+	}
+
+	private String returnCamelCaseByTableName(String input) {
+
+		if (input == null) {
+			return null;
+		}
+		String str = input.trim();
+		if (str.isEmpty()) {
+			return str;
+		}
+
+		// 1) 이상한 구분자 정리
+		str = str.replaceAll("[^A-Za-z0-9]+", "_");
+
+		// 2) 단어 경계 삽입 (약어/연속 대문자 보존)
+		// ...ABCDef -> ABC_Def (약어 + 일반 단어)
+		str = str.replaceAll("([A-Z]+)([A-Z][a-z])", "$1_$2");
+		// ...aB -> a_B (소문자/숫자 + 대문자)
+		str = str.replaceAll("([a-z\\d])([A-Z])", "$1_$2");
+		// 문자/숫자 경계
+		str = str.replaceAll("([A-Za-z])(\\d)", "$1_$2")
+			.replaceAll("(\\d)([A-Za-z])", "$1_$2");
+
+		// 3) 언더스코어 정리
+		str = str.replaceAll("_+", "_")
+			.replaceAll("^_+|_+$", "");
+
+		System.out.println(str);
+
+		System.out.println(str.toUpperCase(Locale.ROOT));
+		return str.toUpperCase(Locale.ROOT);
+
+	}
+}
