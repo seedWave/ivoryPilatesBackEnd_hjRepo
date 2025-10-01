3247b53098e3155459f123b1ab9ad357baaf9013
nks <knsol19921@gmail.com>
2025-10-02 00:20:15 +0900

    Controller REST_API 변경

--- src/main/java/com/dogfeetbirdfeet/ivorypilatesbackend/controller/acct/AcctController.java
+++ src/main/java/com/dogfeetbirdfeet/ivorypilatesbackend/controller/acct/AcctController.java
@@ -3,12 +3,7 @@ package com.dogfeetbirdfeet.ivorypilatesbackend.controller.acct;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.dogfeetbirdfeet.ivorypilatesbackend.component.util.commonmethod.CommonMethod;
 import com.dogfeetbirdfeet.ivorypilatesbackend.component.util.maker.ApiResponse;
@@ -31,7 +26,7 @@ public class AcctController {
 	 * @author nks
 	 * @return 모든 계정 목록
 	 */
-	@GetMapping("/getAllAccts")
+	@GetMapping()
 	public ResponseEntity<List<Acct>> getAllAccts() {
 		return ResponseEntity.ok(acctService.getAllAccts());
 	}
@@ -42,8 +37,8 @@ public class AcctController {
 	 * @param acctId 대상 계정 아이디
 	 * @return 계정 정보
 	 */
-	@GetMapping("/getAcctById")
-	public ResponseEntity<Acct> getAcctById(@RequestParam String acctId) {
+	@GetMapping("/{acctId}")
+	public ResponseEntity<Acct> getAcctById(@PathVariable String acctId) {
 		return ResponseEntity.ok(acctService.getAcctById(acctId));
 	}
 
@@ -55,7 +50,7 @@ public class AcctController {
 	 * @param userId 생성하는 계정 아이디
 	 * @return 생성된 계정 정보
 	 */
-	@PostMapping("/insertAcct")
+	@PostMapping()
 	public ResponseEntity<ApiResponse<Acct>> insertAcct(@RequestBody Acct acct,
 		@RequestParam String userId) {
 		return commonMethod.responseTransaction(acctService.insertAcct(acct, userId));
