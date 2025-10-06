285f2bf9c089716b889456511c0360259a4f8856
nks <knsol19921@gmail.com>
2025-10-06 21:28:08 +0900

    테스트용 컨트롤러 수정

--- src/main/java/com/dogfeetbirdfeet/ivorypilatesbackend/controller/test/TestController.java
+++ src/main/java/com/dogfeetbirdfeet/ivorypilatesbackend/controller/test/TestController.java
@@ -3,38 +3,38 @@ package com.dogfeetbirdfeet.ivorypilatesbackend.controller.test;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.dogfeetbirdfeet.ivorypilatesbackend.dto.searchdto.TestSearchDto;
 import com.dogfeetbirdfeet.ivorypilatesbackend.service.test.TestService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
-    private final TestService testService;
+	private final TestService testService;
 
-    public TestController(TestService testService) {
-        this.testService = testService;
-    }
+	public TestController(TestService testService) {
+		this.testService = testService;
+	}
 
-    /**
-     * 테스트용 모든 테이블 반환
-     *
-     * @param tableName 반환할 테이블 이름 (camelCase)
-     * @param dto       조회 조건
-     * @return 대상 테이블 모든 정보
-     * @
-     */
-    @GetMapping("/{tableName}")
-    public ResponseEntity<List<?>> getTableInfo(@PathVariable String tableName, @ModelAttribute TestSearchDto dto) {
+	/**
+	 * 테스트용 모든 테이블 반환
+	 *
+	 * @param tableName 반환할 테이블 이름 (camelCase)
+	 * @param dto       조회 조건
+	 * @return 대상 테이블 모든 정보
+	 * @
+	 */
+	@GetMapping("/{tableName}")
+	public ResponseEntity<List<?>> getTableInfo(@PathVariable String tableName, @ModelAttribute TestSearchDto dto) {
 
-        System.out.println(tableName);
-        System.out.println(dto);
-        return ResponseEntity.ok(testService.getTableInfo(tableName, dto));
-    }
+		System.out.println(tableName);
+		System.out.println(dto);
+		return ResponseEntity.ok(testService.getTableInfo(tableName, dto));
+	}
 }
